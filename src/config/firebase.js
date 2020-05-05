25b002a9f5a5f4af9ba8a44d6f794ebe71dce88a
Scotty Walters <scottycwalters@gmail.com>
2020-05-05 15:39:59 -0400

    setting up

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,28 @@
 import firebase from 'firebase/app';
-import 'firebase/auth';   
+import 'firebase/auth';
 import 'firebase/storage';
 import 'firebase/firestore';
 import 'firebase/messaging';
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
+//   authDomain: "myaurapp.firebaseapp.com",
+//   databaseURL: "https://myaurapp.firebaseio.com",
+//   projectId: "myaurapp",
+//   storageBucket: "myaurapp.appspot.com",
+//   messagingSenderId: "419511997060",
+//   appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-  authDomain: "myaurapp.firebaseapp.com",
-  databaseURL: "https://myaurapp.firebaseio.com",
-  projectId: "myaurapp",
-  storageBucket: "myaurapp.appspot.com",
-  messagingSenderId: "419511997060",
-  appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+  apiKey: "AIzaSyDtIZCZDhvvu25ECvj47RK0DTKD_SKb5Ms",
+  authDomain: "learnthis-3cd39.firebaseapp.com",
+  databaseURL: "https://learnthis-3cd39.firebaseio.com",
+  projectId: "learnthis-3cd39",
+  storageBucket: "learnthis-3cd39.appspot.com",
+  messagingSenderId: "629359057705",
+  appId: "1:629359057705:web:6e166b559bfbb4bd1cf538",
+  measurementId: "G-XWVE5G3KQK"
 };
 
 firebase.initializeApp(firebaseConfig);
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   productionSourceMap: false,
   pwa: {
-    name: 'Aura Admin',
+    name: 'LernThis Admin',
     workboxPluginMode: 'InjectManifest',
     themeColor: '#4A90E2',
     msTileColor: '#4A90E2',
